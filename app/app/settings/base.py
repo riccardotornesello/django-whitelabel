2e7d757142c68a4fd7e1495880db957546af2b0b
riccardotornesello <riccardo.tornesello@gmail.com>
2022-05-11 20:23:37 +0200

    fix EMAIL_PORT to int

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -81,7 +81,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'localhost')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-EMAIL_PORT = os.environ.get('EMAIL_PORT', 587)
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
 
 LOGGING = {
